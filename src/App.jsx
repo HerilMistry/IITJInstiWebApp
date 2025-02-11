c60c657a40a6e7fdf8a96ce467b82e5d404abfe3
DeveloperAmrit <105799602+DeveloperAmrit@users.noreply.github.com>
2025-02-11 20:41:41 +0530

    modified:   src/App.jsx 	new file:   src/components/VerticalNavbar.jsx 	new file:   src/pages/Home.jsx 	new file:   src/pages/MessMenu.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-
+import React from 'react'
+import VerticalNavbar from './components/VerticalNavbar'
 
 function App() {
 
   return (
-    <div className="bg-green-900">
-        Hello world
+    <div className='flex'>
+      <VerticalNavbar>
+        <h1 className='text-white'>Vertical Navbar</h1>
+      </VerticalNavbar>
+      <div className='w-full bg-gray-200'>
+        <h1 className='text-center'>Main Content</h1>
+      </div>
     </div>
   )
 }
---
+++ src/components/VerticalNavbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VerticalNavbar = ({children}) => {
+  return (
+    <div className='bg-green-900 w-64'>
+        {children}
+    </div>
+  )
+}
+
+export default VerticalNavbar
